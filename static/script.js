4d7226ccb3d1bc14e1e3e8606cb14b193ad2e192
Liam Pewton <liampewton99@gmail.com>
2025-07-05 21:35:45 +0100

    Make form interactive depending on choices

---
+++ static/script.js
@@ -0,0 +1,19 @@
+const parameterOptions = document.getElementsByName('chosenParameter');
+
+parameterOptions.forEach(option => {
+  option.addEventListener('change', () => {
+    if ($('#chosenParameterCheckbox1').prop('checked')) {
+        $('#dayOrHourDiv').attr('class', '');
+        $('#chosenDayDiv').attr('class', 'd-none');
+    }
+  });
+});
+
+parameterOptions.forEach(option => {
+    option.addEventListener('change', () => {
+      if ($('#chosenParameterCheckbox2').prop('checked')) {
+          $('#chosenDayDiv').attr('class', '');
+          $('#dayOrHourDiv').attr('class', 'd-none');
+      }
+    });
+  });
