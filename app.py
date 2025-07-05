027c7cdbcebf5992ec5de029334381765c6d5e0d
Liam Pewton <liampewton99@gmail.com>
2025-07-05 17:23:16 +0100

    Update app.py for render

--- app.py
+++ app.py
@@ -53,11 +53,7 @@ def run_program():
     chosen_day = request.form.get('chosenDay', '')
     chosen_period = request.form.get('chosenPeriod', '0')
 
-    command = ['./cprog', latest_file, chosen_parameter]
-    if chosen_parameter == '0':
-        command.append(chosen_period)
-    else:
-        command.append(chosen_day)
+    command = ['./cprog', latest_file, chosen_day, chosen_period]
 
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
